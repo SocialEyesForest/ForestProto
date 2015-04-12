c867706c11b057463d11fd40f9c29650a683d017
Tito Luyo Murata <tluyo@hotmail.com>
2015-04-11 20:09:58 -0500

    Proyecto inicial (vacio) Web Api C#

---
+++ SocialEyesForest/SocialEyesForest/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SocialEyesForest.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ SocialEyesForest/SocialEyesForest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialEyesForest.WebApiApplication" Language="C#" %>
